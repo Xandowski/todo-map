bfe49cd3e2b012856bc3ab1bcdf8e31fa92fd5c5
Xandowski <xandy-moraes@hotmail.com>
2021-05-15 19:57:20 -0300

    created login page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,49 @@
-import styled from 'styled-components'
+import React, { useEffect } from 'react'
+import Head from 'next/head'
+import Router from 'next/router'
+import { useSession, getSession } from 'next-auth/client'
+import { signIn } from 'next-auth/client'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
 
-export default function Home() {
-  return <Title>My page</Title>
+const LoginPage = () => {
+  const [session, loading] = useSession()
+
+  useEffect(() => {
+    if (session) {
+      Router.push('/home')
+    }
+  }, [session])
+
+  return (
+    <>
+      <Head>
+        <title>Faça o login | todo.map</title>
+      </Head>
+      {(!session && <div>
+        <div>
+          <header>
+            <img src="/images/Logo.svg" alt="" />
+          </header>
+
+          <main>
+            <strong>Bem-vindo</strong>
+            <p>Faça login para continuar</p>
+          </main>
+
+          <div>
+            <button onClick={() => signIn('auth0')}>SignIn</button>
+          </div>
+        </div>
+      </div>) || (loading && <h1>Carrregando...</h1>)}
+    </>
+  )
+}
+
+export default LoginPage
+
+export const getServerSideProps = async (ctx) => {
+  const session = await getSession(ctx)
+  return {
+    props: { session },
+  }
 }
