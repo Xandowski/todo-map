6c1990b083374a1021efe0f54c8d70b24dd1c777
marcosogsantos <marcos_santos100@icloud.com>
2021-05-24 10:40:43 -0300

    fix: wip: Add dayly row to each goal

---
+++ src/pages/api/goals/log.js
@@ -0,0 +1,50 @@
+import connect from '../../../utils/database'
+import Cookies from 'cookies'
+
+export default async (request, response) => {
+  if (request.method === 'GET') {
+
+    const cookies = new Cookies(request, response)
+    const sessionToken = cookies.get('next-auth.session-token')
+
+    if (!sessionToken){
+      response.status(400).json({ message: 'No permission' })
+      return
+    }
+
+    const { db } = await connect()
+    const session = await db.collection('sessions').findOne({ sessionToken: sessionToken })
+
+    if (!session){
+      response.status(400).json({ message: 'No permission' })
+      return
+    }
+
+    var searchParams = {
+      owner: session.userId,
+      createdAt: 
+      {
+        $gte: new Date((new Date().getTime() - (30 * 24 * 60 * 60 * 1000)))
+      }
+    }
+
+    db.collection('goalsLog').find(searchParams).toArray(function (err, docs) { 
+      if (err) {
+        response.status(500).send({error: err})
+        return
+      }
+      if (!docs) {
+        response.status(400).json({ error: 'No goals log was found' })
+        return
+      }
+      response.send(docs)
+      return
+    });
+
+  } else {
+    response.status(400).json({ message: 'Wrong method' })
+    return
+
+  }
+  
+}
--- src/pages/components/ToDoMap.js
+++ src/pages/components/ToDoMap.js
@@ -1,6 +1,37 @@
 import React, { useEffect, useState } from "react";
 import Axios from 'axios'
 
+const getLast30days = () => {
+  var datesArray = []
+  
+  for (let i = 0; i < 30; i++){
+    let date = new Date(new Date().setDate(new Date().getDate() - i))
+    datesArray.push(date)
+  }
+  
+  return datesArray
+}
+
+function sameDay(d1, d2) {
+  return d1.getFullYear() === d2.getFullYear() &&
+    d1.getMonth() === d2.getMonth() &&
+    d1.getDate() === d2.getDate();
+}
+
+const haveDone =  (parentId,goalsLog,dateArrayItem) => {
+  var haveDone = 0
+  goalsLog.forEach((goalLogItem)=>{
+    if (goalLogItem.parentId == parentId && sameDay(dateArrayItem,new Date(goalLogItem.createdAt))){
+      haveDone = 1
+    }
+  })    
+  if (haveDone){
+    return <span parentId={item._id} date={dateArrayItem} >🟩</span>
+  } else {
+    return <span parentId={item._id} data={dateArrayItem}>⬜</span>  
+  }                       
+}
+
 const doneGoal = async event => {
     event.preventDefault()
     
@@ -31,20 +62,37 @@ const ToDoMap = () => {
       setGoals(response.data)    
     }
 
+    const [goalsLog,setGoalsLog]=useState([])
+
+    useEffect(() => { fetchGoalsLog() }, [])
+
+    const fetchGoalsLog=async()=>{
+      const response=await Axios('/api/goals/log');
+      setGoalsLog(response.data)    
+    }
+
     return (
         <>
         <h2>To-Do Map</h2>
         <div>
             {goals.map((item,key)=>{
                 return (
-                    <div key={key}>
-                        {item.name} 
-                        <span>
-                            <button value={item._id} onClick={doneGoal}>Complete it</button>
-                        </span>
-                    </div>
+                  <div key={key}>
+                      {item.name} 
+                      <span>
+                          <button value={item._id} onClick={doneGoal}>Complete it</button>
+                      </span>
+                      {                          
+                      getLast30days().map((dateArrayItem)=>{
+                        return (<span>
+                          {haveDone(item._id,goalsLog,dateArrayItem)}
+                        </span>)
+                      })}
+                  </div>
                 )
-            })}
+              })}
+        </div>
+        <div>
         </div>
         </>
     )
