14bfc52bb96fb5d2491a71641b7b1a0f79747f68
marcosogsantos <marcos_santos100@icloud.com>
2021-05-24 22:59:54 -0300

    fix: Rollback dabase.js

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,39 +1,39 @@
-// import { MongoClient } from 'mongodb'
-
-// const client = new MongoClient(process.env.DATABASE_URL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-
-// export default async function connect() {
-//   if (!client.isConnected()) await client.connect()
-//   const db = client.db('todo-map')
-//   return { db, client }
-
-// }
-
-
 import { MongoClient } from 'mongodb'
 
-let uri = process.env.DATABASE_URL || "" // trick ts :(
-let dbName = 'todo-map'
-
-var cachedClient, cachedDb
+const client = new MongoClient(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
 export default async function connect() {
-  if (cachedClient && cachedDb) {
-    return { client: cachedClient, db: cachedDb }
-  }
-
-  const client = await MongoClient.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-
-  const db = await client.db(dbName)
-
-  cachedClient = client
-  cachedDb = db
-
+  if (!client.isConnected()) await client.connect()
+  const db = client.db('todo-map')
   return { db, client }
+
 }
+
+
+// import { MongoClient } from 'mongodb'
+
+// let uri = process.env.DATABASE_URL || "" // trick ts :(
+// let dbName = 'todo-map'
+
+// var cachedClient, cachedDb
+
+// export default async function connect() {
+//   if (cachedClient && cachedDb) {
+//     return { client: cachedClient, db: cachedDb }
+//   }
+
+//   const client = await MongoClient.connect(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+
+//   const db = await client.db(dbName)
+
+//   cachedClient = client
+//   cachedDb = db
+
+//   return { db, client }
+// }
