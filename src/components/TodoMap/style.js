40911a7c56488e19b7ec8881dc96db4259e62e60
marcosogsantos <marcos_santos100@icloud.com>
2021-05-26 23:20:40 -0300

    fix: Update goal names layout

--- src/components/TodoMap/style.js
+++ src/components/TodoMap/style.js
@@ -14,8 +14,8 @@ vertical-align:top;
 
 export const GoalRow = styled.div`
 white-space:nowrap;
-
 `
+
 export const GoalColumn = styled.div`
 display: inline-block;
 `
@@ -58,19 +58,52 @@ max-width: 700px;
     background-color: #d9d9d9;
 }
 `
-export const NameColumn = styled.div`
-width: 250px;
-margin: 0 20px 0 0;
-text-align: right;
-display: inline-block;
-min-height: 20px;
-font-size: 23px;
-`
+
 export const DailyCell = styled.div`
 border-radius: 0.3em;
 display: inline-block;
 white-space: normal; /*Prevents child elements from inheriting nowrap.*/
-min-height: 22px;
+min-height: 28px;
 min-width: 30px;
 background: ${props => props.done ? "#66ff99" : "#f2f2f2"};
+`
+
+export const NameColumn = styled.div`
+width: 250px;
+margin: 0 5px 0 0;
+text-align: right;
+display: inline-block;
+font-size: 23px;
+`
+
+export const GoalNameButton = styled.button`
+    font-size: 19px;
+    border: 0;
+    background: #ccffcc;
+    border-radius: 15px;
+    padding: 3px 15px 3px 15px;
+    cursor: pointer;
+    float:right;
+    position:relative;
+    &:after{
+        display: none;
+        content: "Done";
+        position:absolute;
+        width: 100%;
+        left: 0;
+        top: 0;
+        background-color: #80ff80;
+        border: 0;
+        border-radius: 15px;
+        cursor: pointer;
+        padding: 3px 0 3px 0;
+        /* color: white; */
+    }
+    &:hover:after {
+        display: block;
+    }
+    &:hover {
+        background: #b3ffb3;
+    }
+    
 `
