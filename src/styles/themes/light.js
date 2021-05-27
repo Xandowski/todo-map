8b1306a3a402d1d097e4d74515dfd105e21d8b50
Xandowski <xandy-moraes@hotmail.com>
2021-05-26 22:45:35 -0300

    fix: change light theme colors

--- src/styles/themes/light.js
+++ src/styles/themes/light.js
@@ -2,15 +2,10 @@ export default {
   title: 'light',
   colors: {
     background: '#F2F3F5',
-    grayLine: '#DcDDE0',
-    white: 'white',
-    text: '#666',
-    title: '#2E384D',
-    red: '#E83F5B',
-    redDark: '#b93248',
-    green: '#4CD62B',
-    blue: '#5965E0',
-    blueDark: '#4953B8',
-    blueTwitter: '#2AA9E0',
+    primary: '#264653',
+    secondary: '#D7EBEC',
+    success: '#2A9D8F',
+    failed: '#E76F51',
+    white: '#fff'
   },
 }
