3dd147bcd0465427737fe384714f1fa236a29179
marcosogsantos <marcos_santos100@icloud.com>
2021-07-18 10:04:15 -0300

    fix: Dynamic route loading resources twice

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,12 +9,10 @@ const Home = () => {
   const [session, loading] = useSession()
 
   useEffect(() => {
-    if (!session) {
-      setTimeout(() => {
-        Router.push('/')
-      }, 1000)
+    if (!loading && !session) {
+      Router.push('/')
     }
-  }, [])
+  }, [loading])
 
   return (
     <>
@@ -22,7 +20,7 @@ const Home = () => {
         <title>Todo Map</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+        <link rel="preload" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
       </Head>
       {(session && (
         <>
